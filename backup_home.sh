f6af9b7c7ae9040c390a05ed4e65dbd378584cd4
S11ker <richard.saifier.87@gmail.com>
2024-03-10 21:28:52 +0300

    Backup with date, using bz2, in the backups_home directory, with log rotation (4 backups - maximum)

---
+++ backup_home.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DATE=$(date +"Date(%d_%m_%Y)_Time(%H_%M)")
+
+mkdir $HOME/backups_home
+
+cd "$HOME"/backups_home || exit
+
+COUNT_FILES=$(ls | wc -l)
+
+if [ "$COUNT_FILES" -eq 4 ]; then
+	THE_OLDEST_BACKUP=$(ls -tr | head -n 1)
+	rm -f "$THE_OLDEST_BACKUP"
+fi
+
+tar -cjf "$(whoami)_home_$DATE.tb2" "$HOME" 2>/dev/null
+
+exit 0
